ebe553ef4b72d7da95c467743e2396a6c771af04
joão.jonny <138599648+progcienti@users.noreply.github.com>
2025-02-07 15:07:38 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -17,4 +17,17 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
-  
+
+function showProjectDetails(projectId) {
+    let details = {
+      'project1': '<h2>Impressora 3D de Baixo Custo</h2><p>A proposta da impressora 3D de baixo custo da InnoAcre é criar uma solução acessível, sustentável e funcional, utilizando componentes reaproveitados e de fácil obtenção. Nossa impressora é projetada para ser montada com materiais acessíveis, aproveitando peças recicladas e adaptadas, sem comprometer a qualidade das impressões. Essa proposta reflete a evolução das tecnologias de manufatura aditiva e a capacidade de integrar inovação com sustentabilidade.</p>',
+      'project2': '<h2>Máquina de Filamentos Reciclados</h2><p>A proposta da Máquina de Filamentos da InnoAcre é criar uma solução inovadora e sustentável para a fabricação de filamentos para impressão 3D utilizando garrafas PET. Essa abordagem promove a reutilização de materiais plásticos descartados, reduzindo o impacto ambiental e fornecendo uma alternativa acessível para a produção de filamentos de alta qualidade.</p>'
+    };
+    
+    document.getElementById('projectDetails').innerHTML = details[projectId];
+    document.getElementById('projectModal').style.display = 'block';
+  }
+
+  function closeModal() {
+    document.getElementById('projectModal').style.display = 'none';
+  }
